d03ecc97b5d8032f5bca180113489a2f329915ab
BETO <BETO@DESKTOP-JSCHLEC>
2021-02-28 23:16:52 -0600

    Actividad 5 28/2/2021

---
+++ Actividad 5/SimuladorInversion.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace Actividad_5
+{
+    class SimuladorInversion:INotifyPropertyChanged
+    {
+        private decimal monto;
+
+        private short plazo;
+
+        public decimal MontoInvertir
+
+        {
+
+            get { return monto; }
+
+            set { monto = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(null)); }
+
+        }
+
+        public short PlazoAnual
+
+        {
+
+            get { return plazo; }
+
+            set { plazo = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(null)); }
+
+        }
+
+        public decimal Total
+
+        {
+
+            get
+
+            {
+
+                decimal CantidadTotal = monto;
+
+                for (int i = 0; i != plazo; i++)
+
+                {
+
+                    CantidadTotal += (CantidadTotal * 0.07m);
+
+                    CantidadTotal = (CantidadTotal > 30000) ? CantidadTotal - (CantidadTotal * 0.02m) : CantidadTotal;
+
+                }
+
+                return CantidadTotal;
+
+            }
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
